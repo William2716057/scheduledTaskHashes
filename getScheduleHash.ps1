94a8e121fa6bc0bc88851b4fe2fd7d220df830a5
User <Willspri@gmail.com>
2024-08-21 13:42:55 +1200

    save to csv

--- getScheduleHash.ps1
+++ getScheduleHash.ps1
@@ -1,16 +1,15 @@
 Get-ScheduledTask | ForEach-Object {
-	$task = $_
-	$actions = $task.Actions.Execute
-	$hash = if (Test-Path $actions) {
-		Get-FileHash -Algorithm SHA256 $actions | Select-Object -ExpandProperty Hash
-	} else {
-		"N/A"
-	}
-
-	[PSCustomObject]@{
-		TaskName	= $task.TaskName
-		State		= $task.State
-		LastRunTime	= $task.LastRunTime
-		FileHash	= $hash
-	}
-} | Format-Table -Autosize
+    $task = $_
+    $actions = $task.Actions.Execute
+    $hash = if (Test-Path $actions) {
+        Get-FileHash -Algorithm SHA256 $actions | Select-Object -ExpandProperty Hash
+    } else {
+        "N/A"
+    }
+    [PSCustomObject]@{
+        TaskName     = $task.TaskName
+        State        = $task.State
+        LastRunTime  = $task.LastRunTime
+        FileHash     = $hash
+    }
+} | Export-Csv -Path "results.csv" -NoTypeInformation
